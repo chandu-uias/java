07908b3a4dcd9f79164307e78828bc955e861b8f
chandu-uias <146953506+chandu-uias@users.noreply.github.com>
2023-10-04 05:47:19 -0700

    Add files via upload

---
+++ binarynumbers.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e5+10;
+int main()
+{
+    cout<<INT_MAX<<endl;
+    int a=(1LL<<31)-1;//to get power of 2
+    cout<<a<<endl;
+    unsigned int b=(1LL<<32)-1;
+    cout<<b;
+}
